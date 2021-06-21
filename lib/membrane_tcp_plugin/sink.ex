fbde11d5c12e5454e8cebea6fe3550bc2338f6cf
Piotrek Żyła <piotr.zyla@swmansion.com>
2021-06-21 13:08:54 +0200

    Initial working version of a TCP source and sink (#1)
    * Initial working version of a TCP source and sink
    
    Co-authored-by: Daniel Jodłoś <danjod40@gmail.com>

---
+++ lib/membrane_tcp_plugin/sink.ex
@@ -0,0 +1,38 @@
+defmodule Membrane.TCP.Sink do
+  @moduledoc """
+  Element that sends received buffers to a created TCP socket.
+  """
+  use Membrane.Sink
+
+  alias Membrane.Buffer
+
+  def_options ip_address: [
+                type: :string,
+                description: "Destination IP address to establish TCP connection with"
+              ],
+              port_no: [spec: 0..65535, description: "Port number"]
+
+  def_input_pad :input,
+    caps: :any,
+    demand_unit: :buffers
+
+  @impl true
+  def handle_init(%__MODULE__{ip_address: ip_address, port_no: port_no}) do
+    {:ok, socket} = :gen_tcp.connect(ip_address, port_no, [:binary, active: false])
+
+    {:ok, %{socket: socket}}
+  end
+
+  @impl true
+  def handle_prepared_to_playing(_context, state) do
+    {{:ok, demand: :input}, state}
+  end
+
+  @impl true
+  def handle_write(:input, %Buffer{payload: payload}, _context, %{socket: socket} = state) do
+    case :gen_tcp.send(socket, payload) do
+      :ok -> {{:ok, demand: :input}, state}
+      {:error, reason} -> {{:error, reason}, state}
+    end
+  end
+end
